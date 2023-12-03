7b16e94f9f4f8e9c4c29214d459968d16be50b8f
KoplanyiDavid <55060571+KoplanyiDavid@users.noreply.github.com>
2023-12-03 13:52:30 +0100

    make Music class to inherit from SE class as have same methods

--- src/Main/Music.java
+++ src/Main/Music.java
@@ -1,27 +1,8 @@
 package Main;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import java.net.URL;
 
-public class Music {
-    Clip clip;
-    public void setFile(URL name) {
-        try {
-            AudioInputStream sound = AudioSystem.getAudioInputStream(name);
-            clip = AudioSystem.getClip();
-            clip.open(sound);
-        } catch (Exception e) {
-
-        }
-    }
-
-    public void play() {
-        clip.setFramePosition(0);
-        clip.start();
-    }
-
+public class Music extends SE {
     public void loop() {
         clip.loop(Clip.LOOP_CONTINUOUSLY);
     }
